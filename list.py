418798f5ed0c4b5942c8ca87f5b5d65002b8c06f
srijasavara2005-dot <srijasavara2005@gmail.com>
2026-06-30 12:08:42 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,18 @@
+my_list = [1, 2, 3]
+print("Create List:", my_list)
+
+my_list.append(4)
+print("Append:", my_list)
+
+my_list.insert(0, 0)
+print("Insert:", my_list)
+
+my_list.remove(2)
+print("Remove:", my_list)
+
+print("Index Access:", my_list[0])
+
+print("Slicing:", my_list[1:3])
+
+my_list.pop()
+print("Pop:", my_list)
